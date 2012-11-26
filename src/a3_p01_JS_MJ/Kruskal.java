a85137d2b2b3885292187b3b254ec26d43975766
redvox <redpaperfox@gmail.com>
2012-11-26 17:59:35 +0100

    First commit of exercise 3 to add the packages.

---
+++ src/a3_p01_JS_MJ/Kruskal.java
@@ -0,0 +1,5 @@
+package a3_p01_JS_MJ;
+
+public class Kruskal {
+
+}
---
+++ src/a3_p01_JS_MJ_test/a3Test.java
@@ -0,0 +1,14 @@
+package a3_p01_JS_MJ_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class a3Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
