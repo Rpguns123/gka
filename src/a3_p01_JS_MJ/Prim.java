81c9da132a10f1a443bcc892442e06fa6459c7af
MJ <marcus.jenz@gmail.com>
2012-11-27 17:27:19 +0100

    Prim with PrioQueue continued
    Hab den Prim weitergemacht. Der müsste so gut wie fertig sein. Ich muss
    dann nur noch mal in Util schauen wie ich am besten die Kreise finde,
    sodass ich den Algorithmus testen kann.

--- src/a3_p01_JS_MJ/Prim.java
+++ src/a3_p01_JS_MJ/Prim.java
@@ -2,6 +2,8 @@ package a3_p01_JS_MJ;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
@@ -17,36 +19,96 @@ public class Prim {
 	{
 		WeightedGraph<AttributedNode<String>,DefaultWeightedEdge> newGraph = new WeightedPseudograph<AttributedNode<String>, DefaultWeightedEdge>(DefaultWeightedEdge.class);
 		AttributedNode<String> sourceNode = firstNode;
-		//Set<AttributedNode<String>> s = graph.vertexSet();
+		newGraph.addVertex(sourceNode);
+		Set<AttributedNode<String>> nodeSet = graph.vertexSet();
+		int nodeCount = nodeSet.size();
 		
-		List<AttributedNode<String>> neigh = Graphs.neighborListOf(graph, firstNode);
+		List<AttributedNode<String>> neigh; 
 		List<DefaultWeightedEdge> l = new ArrayList<DefaultWeightedEdge>();
+		PriorityQueue<QueueEntry> prioQueue = new PriorityQueue<QueueEntry>();
 		/* Die Anzahl der Knoten prüfen, und für jeden Knoten einen Zähler um eins hochzählen
 		 * Dann stoppen wenn die Zähler = Anzahl Knoten
 		 * --> For Schleife.
 		 */		
-		for (AttributedNode<String> node : neigh)//Alle Kanten suchen die von uns abgehen.
+		for (int count =  1; count < nodeCount;)
 		{
-			l.add(graph.getEdge(sourceNode, node));
-		}
-		double min = Double.MAX_VALUE;
-		DefaultWeightedEdge nextEdge=null;
-		for (DefaultWeightedEdge e : l)//Minimale Edge suchen.
-		{
-			if (graph.getEdgeWeight(e)<min)
+			neigh = Graphs.neighborListOf(graph, sourceNode);
+			neigh.retainAll(nodeSet);//Wir benutzen nur die Knoten die noch nciht im Baum drin sind.
+			//Also die Schnittmenge der Nachbarn udn der noch nicht vorhandenen Knoten.
+			
+			for (AttributedNode<String> node : neigh)//Alle Kanten suchen die von uns abgehen.
 			{
-				nextEdge = e;
+				prioQueue.add(getQueueEntry(graph.getAllEdges(sourceNode, node),graph));//Falls wir PArallelen haben muessen wuir alle Edges hinzufuegen.
 			}
+			QueueEntry q = prioQueue.poll();
+			graph.addVertex(q.getTargetNode());
+			graph.addEdge(q.getSourceNode(), q.getTargetNode(), q.getEdge());
+			graph.setEdgeWeight(q.getEdge(), q.getWeight());
+			//Graphs.addEdgeWithVertices(newGraph, graph, q.getEdge());
+			if(Util.checkForCircles(newGraph))
+			{
+				newGraph.removeEdge(q.getEdge());
+			}else {
+				count++;
+				nodeSet.remove(q.getTargetNode());
+			}
+			sourceNode=q.getTargetNode();
 		}
-		l.remove(nextEdge);//Hier könnte u.U. eine nullPointerException auftauchen, wenn keine gewichte kleine sind als Double.MAX_VALUE
-		Graphs.addEdgeWithVertices(newGraph, graph, nextEdge);
-		if(checkForCircles(newGraph))
+		return newGraph;
+	}
+	
+	private static QueueEntry getQueueEntry(Set<DefaultWeightedEdge> set, WeightedGraph<AttributedNode<String>, DefaultWeightedEdge> g)
+	{
+		double weight = Double.MAX_VALUE;
+		DefaultWeightedEdge edge = null;
+		for(DefaultWeightedEdge e : set)
 		{
-			;//Kante wieder wegnehmen.
-		}
-		sourceNode=graph.getEdgeTarget(nextEdge);
-		
-		return null;
+			double d = g.getEdgeWeight(e);
+			if (d<weight)
+			{
+				edge = e;
+				weight = d;
+			}
+		}		
+		return new QueueEntry(edge,g.getEdgeSource(edge), g.getEdgeTarget(edge), weight);
 	}
 
+}
+
+class QueueEntry implements Comparable<QueueEntry>{
+	private DefaultWeightedEdge edge;
+	private AttributedNode<String> targetNode,sourceNode;
+	private double weight;
+	QueueEntry(DefaultWeightedEdge e,AttributedNode<String> sourceNode, AttributedNode<String> targetNode, double weight)
+	{
+		this.sourceNode = sourceNode;
+		this.edge = e;
+		this.targetNode = targetNode;
+		this.weight = weight;
+	}
+
+	public AttributedNode<String> getSourceNode() {
+		return sourceNode;
+	}
+
+	@Override
+	public int compareTo(QueueEntry o) {
+		return (int)(this.weight-o.weight);
+		
+	}
+	
+	DefaultWeightedEdge getEdge()
+	{
+		return edge;
+	}
+	
+	AttributedNode<String> getTargetNode()
+	{
+		return targetNode;
+	}
+	double getWeight()
+	{
+		return weight;
+	}
+	
 }
