a89981ec6a44da79084cc22ba564faf130475423
redvox <redpaperfox@gmail.com>
2012-12-19 15:03:50 +0100

    Fleury complete, untested.

--- src/a4_p01_JS_MJ/Fleury.java
+++ src/a4_p01_JS_MJ/Fleury.java
@@ -1,5 +1,79 @@
 package a4_p01_JS_MJ;
 
-public class Fleury {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import org.jgrapht.Graph;
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.WeightedGraph;
+import org.jgrapht.alg.EulerianCircuit;
+import org.jgrapht.graph.AbstractBaseGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.Pseudograph;
+
+import a1_p01_JS_MJ.Breadth_First_Search;
+import a1_p01_JS_MJ.Depth_First_Search;
+import a1_p01_JS_MJ.SearchResult;
+
+public class Fleury<T extends Comparable<T>> {
+
+	public SearchResult search(Pseudograph<T, DefaultEdge> graph){
+		// Result
+		ArrayList<DefaultEdge> eulerianpath = new ArrayList<DefaultEdge>(); 
+		int overalledgecount = graph.edgeSet().size();
+		int accesses = 0;
+		
+		// Random Startpoint
+		T vertex = random(graph.vertexSet());
+		accesses++;
+		
+		while(eulerianpath.size() != overalledgecount){
+			Set<DefaultEdge> edges = graph.edgesOf(vertex);
+			Set<DefaultEdge> unmakededges = new HashSet<DefaultEdge>();
+			for(DefaultEdge e : edges){
+				if(!eulerianpath.contains(e)) unmakededges.add(e);
+				accesses++;
+			}
+			
+			if(unmakededges.size() == 0) break;
+			
+			DefaultEdge choosenedge;
+			while(true){
+				accesses++;
+				choosenedge = random(unmakededges);
+				if(unmakededges.size() == 1 || isNotBridgeEdge(graph, eulerianpath, choosenedge)) break;
+				unmakededges.remove(choosenedge);
+			}
+			eulerianpath.add(choosenedge);
+		}
+		
+		return new SearchResult(graph, eulerianpath, null, accesses);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public boolean isNotBridgeEdge(Pseudograph<T, DefaultEdge> graph, ArrayList<DefaultEdge> eulerianpath,  DefaultEdge edge){
+		Pseudograph<T, DefaultEdge> g2 = (Pseudograph<T, DefaultEdge>) graph.clone();
+		g2.removeEdge(edge);
+		// Unmarked Graph
+		for(DefaultEdge e : eulerianpath) g2.removeEdge(e);
+		SearchResult result = Breadth_First_Search.searchShortestPath((AbstractBaseGraph<String, DefaultEdge>) g2, (String) graph.getEdgeSource(edge), (String) graph.getEdgeTarget(edge));
+		
+		if(result.getPath() != null) return true;
+		return false;
+	}
+	
+	public T random(Set<T> set){
+		ArrayList<T> arraylist = new ArrayList<T>();
+		arraylist.addAll(set);
+		return  arraylist.get((int) Math.round(Math.random()*arraylist.size()));
+	}
+	
+	public DefaultEdge random(Set<DefaultEdge> set){
+		ArrayList<DefaultEdge> arraylist = new ArrayList<DefaultEdge>();
+		arraylist.addAll(set);
+		return  arraylist.get((int) Math.round(Math.random()*arraylist.size()));
+	}
 }
