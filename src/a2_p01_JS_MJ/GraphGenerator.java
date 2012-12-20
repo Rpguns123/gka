8c7427f1934a70f12f0f57cff904342dee298664
redvox <redpaperfox@gmail.com>
2012-12-20 13:33:53 +0100

    Added triangle graph generator.

--- src/a2_p01_JS_MJ/GraphGenerator.java
+++ src/a2_p01_JS_MJ/GraphGenerator.java
@@ -1,11 +1,16 @@
 package a2_p01_JS_MJ;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.WeightedGraph;
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.Pseudograph;
 import org.jgrapht.graph.WeightedPseudograph;
 
 public class GraphGenerator {
@@ -79,6 +84,54 @@ public class GraphGenerator {
        
         return graph;
     }
+    
+    public static Pseudograph<String, DefaultEdge> generateEulBig(int nodeCount){
+    	Pseudograph<String, DefaultEdge> graph = new Pseudograph<String, DefaultEdge>(DefaultEdge.class);
+    	
+    	ArrayList<String> blatter = new ArrayList<String>();
+    	
+    	String name1 = Integer.toString((int)(Math.random()*10000));
+		String name2 = Integer.toString((int)(Math.random()*10000));
+		String name3 = Integer.toString((int)(Math.random()*10000));
+		
+		blatter.add(name1);
+		blatter.add(name2);
+		blatter.add(name3);
+    	
+		graph.addVertex(name1);
+		graph.addVertex(name2);
+		graph.addVertex(name3);
+		
+		graph.addEdge(name1, name2);
+		graph.addEdge(name2, name3);
+		graph.addEdge(name3, name1);
+		
+    	for(int i=0; i<nodeCount;i++){
+    		int index = random(blatter);
+    		
+    		String v1 = blatter.get(index);
+    		String v2 = Integer.toString((int)(Math.random()*10000));
+    		String v3 = Integer.toString((int)(Math.random()*10000));
+    		
+    		
+    		blatter.remove(index);
+    		blatter.add(v2);
+    		blatter.add(v3);
+    		
+    		graph.addVertex(v2);
+    		graph.addVertex(v3);
+    		
+    		graph.addEdge(v1, v2);
+    		graph.addEdge(v2, v3);
+    		graph.addEdge(v3, v1);
+    	}
+    	
+    	return graph;
+    }
    
+    
+	public static int random(ArrayList<String> arraylist){
+		return (int) Math.round(Math.random()*(arraylist.size()-1));
+	}
 }
 
